1725a2e48f291919e6f0b24b0eff32b403188a4d
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-11-18 11:13:24 -0500

    Initial Commit

---
+++ HappyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyOrSad
+//
+//  Created by Mohamed, Ilana on 2019-11-18.
+//  Copyright © 2019 Mohamed, Ilana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
