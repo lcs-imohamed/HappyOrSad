a70510e43a485da6c3012614421ebedc1174a759
Ilana Mohamed <Imohamed@lcs.on.ca>
2019-11-21 19:11:18 -0500

    Finished logic to check for happy and sad emojis in user input. Created variables to keep track of happy/sad count.

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
             happyOrSadOutput.text = "Please input a value. "
             return
         }
-       
+        
         
         //Prevent user from entering a message that is too long
         if phraseInput.count > 255 {
@@ -40,25 +40,28 @@ class ViewController: UIViewController {
             return
         }
         
-        
+        //Create variables to count number of happy and sad emojis
         var happyCount = 0
         var sadCount = 0
-       for singleCharacter in phraseInput {
-        if singleCharacter == "😃" || singleCharacter == "😊" || singleCharacter == "🙂" {
-            happyCount += 1
-        }
-        for singleCharacter == "
-
         
-           
+        //Check each phrase for happy or sad emojis. If happy emojis are found, increase happy count by 1. Do the same for sad emojis
+        for singleCharacter in phraseInput {
+            if singleCharacter == "😃" || singleCharacter == "😊" || singleCharacter == "🙂" || singleCharacter == "😄" {
+                happyCount += 1
+            }
+            for singleCharacter in phraseInput {
+                if singleCharacter == "☹" || singleCharacter == "🙁" || singleCharacter == "😕" || singleCharacter == "😔" {
+                sadCount += 1
+                }
             
-        //use happy count and sad count to track counts
-        //after inspcting each character
-        //if/else if............
-        //if cond
-        //else if cond
-    }
-        print(happyCount)
     
+            }
+         //use happy count and sad count to track counts
+                        //after inspcting each character
+                        //if/else if............
+                        //if cond
+                        //else if cond
+            
+        }
 }
 }
